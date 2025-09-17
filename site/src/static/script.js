8e0378554f1e3c34b30d86c1b5fa48dc63bcccf6
Rik Huijzer <github@huijzer.xyz>
2025-09-17 08:19:07 +0200

    feat: can receive text from core

--- site/src/static/script.js
+++ site/src/static/script.js
@@ -1,5 +1,40 @@
+function writeToPtr(instance, ptr, text) {
+    const buffer = instance.exports.memory.buffer;
+    const view = new Uint8Array(buffer, ptr, 1024);
+    const encoder = new TextEncoder();
+    const with_stop = text + "<END>";
+    view.set(encoder.encode(with_stop));
+}
+
+function readFromPtr(instance, ptr) {
+    const buffer = instance.exports.memory.buffer;
+    const view = new Uint8Array(buffer, ptr, 1024);
+    const length = view.findIndex(byte => byte === 0);
+    const decoder = new TextDecoder();
+
+    return decoder.decode(new Uint8Array(buffer, ptr, length));
+}
+
+function pass(core) {
+  const exports = core.instance.exports;
+  console.log("Pass");
+  const json = JSON.stringify({
+    message: "Pass",
+  });
+  console.log(json);
+  const ptr = exports.alloc();
+  writeToPtr(core.instance, ptr, json);
+  exports.hi(ptr);
+  const result = readFromPtr(core.instance, ptr);
+  exports.dealloc(ptr);
+  console.log(`Received: ${result}`);
+}
+
 WebAssembly.instantiateStreaming(fetch("core.wasm")).then(
-  (results) => {
+  (core) => {
     console.log("Loading of the PrintPilot WebAssembly library succeeded");
+    console.log(core);
+    console.table(Object.getOwnPropertyNames(core.instance.exports))
+    pass(core);
   },
 );

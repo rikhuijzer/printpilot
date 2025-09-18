18368c017930015045d7b95dd8e41df3a36563a6
Rik Huijzer <github@huijzer.xyz>
2025-09-18 10:51:09 +0200

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 use web_sys::window;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format!($($t)*)))
+}
 
 fn main() {
     console_error_panic_hook::set_once();
@@ -10,4 +21,9 @@ fn main() {
     let text_node = document.create_text_node("Hello, world from Vanilla Rust 2!");
     body.append_child(text_node.as_ref())
         .expect("Failed to append text");
+}
+
+#[wasm_bindgen]
+pub fn submit_body_upload() {
+    console_log!("hi 3");
 }

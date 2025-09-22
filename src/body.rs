44f50ff2ce75156d388ef02fa9e700a24e282b34
Rik Huijzer <github@huijzer.xyz>
2025-09-22 16:32:45 +0200

    feat: modify pdf

--- src/body.rs
+++ src/body.rs
@@ -2,6 +2,12 @@ use lopdf::Document;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::Request;
+use lopdf::content::Content;
+use lopdf::content::Operation;
+use lopdf::dictionary;
+use lopdf::Object;
+use lopdf::Stream;
+use std::collections::HashMap;
 use web_sys::RequestInit;
 use web_sys::RequestMode;
 use web_sys::Response;
@@ -40,25 +46,111 @@ struct BodyUpload {
     data: Vec<u8>,
 }
 
+fn convert_to_zero_based(i: i64) -> i64 {
+    i - 1
+}
+
+fn ceildiv(a: u64, b: u64) -> u64 {
+    u64::div_ceil(a, b)
+}
+
+fn pages_indices(doc: &Document) -> HashMap<u32, (u32, u16)> {
+    let mut pages_indices = HashMap::new();
+    for (i, page_id) in doc.get_pages() {
+        pages_indices.insert(i, page_id);
+    }
+    pages_indices
+}
+
+fn python_range(end: u64) -> Vec<u64> {
+    (0..end).collect()
+}
+
+#[test]
+fn test_python_range() {
+    assert_eq!(python_range(0), Vec::new());
+    assert_eq!(python_range(5), vec![0, 1, 2, 3, 4]);
+}
+
+fn blank_page() -> Vec<u8> {
+    vec![]
+}
+
+fn fake_document() -> Document {
+    let mut doc = Document::with_version("1.5");
+    let pages_id = doc.new_object_id();
+    let font_id = doc.add_object(dictionary! {
+        "Type" => "Font",
+        "Subtype" => "Type1",
+        "BaseFont" => "Courier",
+    });
+    let resources_id = doc.add_object(dictionary! {
+        "Font" => dictionary! {
+            "F1" => font_id,
+        },
+    });
+    let content = Content {
+        operations: vec![
+            Operation::new("BT", vec![]),
+            Operation::new("Tf", vec!["F1".into(), 48.into()]),
+            Operation::new("Td", vec![100.into(), 600.into()]),
+            Operation::new("Tj", vec![Object::string_literal("Hello World!")]),
+            Operation::new("ET", vec![]),
+        ],
+    };
+    let content_id = doc.add_object(Stream::new(dictionary! {}, content.encode().unwrap()));
+    let page_id = doc.add_object(dictionary! {
+        "Type" => "Page",
+        "Parent" => pages_id,
+        "Contents" => content_id,
+        "Resources" => resources_id,
+        "MediaBox" => vec![0.into(), 0.into(), 595.into(), 842.into()],
+    });
+    let pages = dictionary! {
+        "Type" => "Pages",
+        "Kids" => vec![page_id.into()],
+        "Count" => 1,
+    };
+    doc.objects.insert(pages_id, Object::Dictionary(pages));
+    let catalog_id = doc.add_object(dictionary! {
+        "Type" => "Catalog",
+        "Pages" => pages_id,
+    });
+    doc.trailer.set("Root", catalog_id);
+
+    doc
+}
+
 fn process_body_upload(body_upload: BodyUpload) -> Vec<u8> {
     let data = body_upload.data;
     console_log!("Data len: {:?}", data.len());
     let mut doc = Document::load_mem(&data).unwrap();
-    let n_pages = doc.get_pages().len().max(10);
-    for (_, page_id) in doc.get_pages().iter().take(n_pages) {
-        let page_dict = doc
-            .get_object_mut(*page_id)
-            .and_then(|obj| obj.as_dict_mut())
-            .expect("Missing page!");
+    let n = doc.get_pages().len();
+    console_log!("n: {:?}", n);
+    let half = ceildiv(n as u64, 2);
+    // If n is odd, we need to add one since we are printing duplex.
+    // Can lead to 3 empty pages in the worst case.
+    let half = if n % 2 == 0 { half } else { half + 1 };
+    console_log!("half: {:?}", half);
+    let indices = &pages_indices(&doc);
 
-        // Get the current rotation if any; the default is 0
-        let current_rotation = page_dict
-            .get(b"Rotate")
-            .and_then(|obj| obj.as_i64())
-            .unwrap_or(0);
-
-        // Add the angle and update
-        page_dict.set("Rotate", (current_rotation + 180) % 360);
+    let mut out = fake_document();
+    let pages_id = out.new_object_id();
+    for i in python_range(ceildiv(half, 2)) {
+        console_log!("i: {:?}", i);
+        // half + 2 : 2 : n
+        let left_index = (half + 2) as u32 + (2 * i as u32);
+        console_log!("left_index: {:?}", left_index);
+        // 2 : 2 : half
+        // let right_index = convert_to_zero_based(2 + (2 * i));
+        let left_content = if left_index < n as u32 {
+            let left_page_id = indices[&left_index].clone();
+            doc.get_page_content(left_page_id).unwrap()
+        } else {
+            blank_page()
+        };
+        // let some_page_id = indices[&1];
+        out.add_page_contents(pages_id, left_content).unwrap();
     }
     let mut target = Vec::new();
     doc.save_to(&mut target).unwrap();

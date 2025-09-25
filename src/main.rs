1bea68e6305e2fb044e87a02848002e06b35e115
Rik Huijzer <github@huijzer.xyz>
2025-09-25 12:34:19 +0200

    ...

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,6 @@ pub fn reset_body_upload() {
     input.set_value("");
 }
 
-#[wasm_bindgen]
-pub fn create_cover() {
-    let document = window().unwrap().document().unwrap();
-    let cover_title = document.get_element_by_id("cover-title").unwrap();
-    let title = cover_title.inner_html();
-}
-
 fn main() {
     console_error_panic_hook::set_once();
     console_log!("Library loaded successfully");
